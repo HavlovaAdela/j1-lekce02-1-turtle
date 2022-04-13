86ff2c198759ffc3f5de2aaea1ef6481b4066465
Adéla Havlová <adhavlova@seznam.cz>
2022-04-13 19:33:40 +0200

    Želva nakreslí domeček.

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,7 +8,18 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+        for (int i = 0; i < 4; i++) {
+            zofka.move(100);
+            zofka.turnRight(90);
+        }
+        zofka.penUp();
         zofka.move(100);
+        zofka.penDown();
+        zofka.turnRight(45);
+        zofka.move(75);
+        zofka.turnRight(90);
+        zofka.move(75);
+
 
     }
 
