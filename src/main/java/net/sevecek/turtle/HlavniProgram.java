10bcbd99821f2103feffafafa3359a652dcadb62
Adéla Havlová <adhavlova@seznam.cz>
2022-04-13 20:09:32 +0200

    Želva nakreslí domeček - nová metoda.

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,21 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+      nakresliDomecek(zofka);
+      //zacatek kresleni prasatka
+//zofka.setLocation(300,300);
+//zofka.move (50);
+//zofka.turnLeft(90);
+//zofka.move(100);
+//zofka.turnLeft(90);
+//zofka.move(50);
+
+
+
+    }
+private void nakresliDomecek(Turtle zofka){
+    for (int j = 0; j < 2; j++) {
+
         for (int i = 0; i < 4; i++) {
             zofka.move(100);
             zofka.turnRight(90);
@@ -19,8 +34,13 @@ public class HlavniProgram {
         zofka.move(75);
         zofka.turnRight(90);
         zofka.move(75);
-
-
+        zofka.penUp();
+        zofka.turnLeft(-45);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.penDown();
     }
-
+}
 }
